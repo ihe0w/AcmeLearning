bb0c9acac0fd4fa1fa9fcd32c1a7e9d03bc45aba
ihe0w <w17680271847@163.com>
2018-06-14 13:56:24 +0800

    just test

--- app/src/main/java/com/example/ihe/acmelearning/MainActivity.java
+++ app/src/main/java/com/example/ihe/acmelearning/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null)
             actionBar.hide();
-
+      String mau="juihy";
 
 
 
